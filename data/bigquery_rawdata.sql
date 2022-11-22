20c3a48b054966f4531a5703f8a48b4a1baf6b97
Murali Mohana Krishna Dandu <murali.munna94@gmail.com>
2022-11-21 23:14:41 -0800

    added src preprocess files

--- data/bigquery_rawdata.sql
+++ data/bigquery_rawdata.sql
@@ -31,3 +31,32 @@ EXISTS (SELECT *
 -- from bigquery-public-data.stackoverflow.posts_questions
 -- group by 1
 -- order by 1
+
+
+WITH questions AS
+(
+select
+id, title, accepted_answer_id, answer_count, comment_count, creation_date, favorite_count, owner_user_id, 
+score, tags, view_count
+from bigquery-public-data.stackoverflow.posts_questions
+where 
+-- tags like '%c%'
+-- REGEXP_CONTAINS(tags, 
+-- 'python|java|go|rust|pandas|numpy|scipy|scikit-learn|pytorch|tensorflow|keras|spacy|nltk|opencv|huggingface|matplotlib|seaborn|azure|google-cloud-platform|aws|docker|airflow|mlflow'
+-- )
+EXISTS (SELECT *
+              FROM UNNEST(split(tags,'|')) AS t
+              WHERE t in (
+                'python','c', 'c++', 'java','go','rust',
+                'data-science', 'machine-learning', 'deep-learning', 'computer-vision', 'artificial-intelligence', 'nlp','reinforcement-learning',
+              'pandas','numpy', 'dask', 'pyspark', 'hadoop', 'spark', 'scipy', 'statsmodels', 'scikit-learn','pytorch','tensorflow','keras',
+              'spacy','nltk','opencv','huggingface','matplotlib','seaborn', 'fastai',
+              'bokeh', 'pyviz', 'pycaret', 'plotly', 'visualization', 'cloud', 'bigdata',
+              'azure','google-cloud-platform','aws', 'mlops', 'amazon-web-services', 'gcp',
+              'docker','airflow','mlflow', 'kubeflow')
+      )
+)
+select a.*, b.location as q_user_location,
+from questions a 
+left join bigquery-public-data.stackoverflow.users as b
+on a.owner_user_id = b.id
