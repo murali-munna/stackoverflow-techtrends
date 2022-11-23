3272ce134347aeaecc103fc3ac90d50700b99a2b
HaoQiuUCSD <h3qiu@ucsd.edu>
2022-11-22 21:29:45 -0800

    smoothing plot

---
+++ src/plot_smooth/ML Ops.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+
+def process(tag_trends_python):
+    a_tags = list(tag_trends_python["tags2"])
+    set(a_tags)
+    
+#     a_tags.remove('pandas')
+    tagsTwo = []
+    questions = []
+    date = []
+    for t in a_tags:
+        tt = tag_trends_python[tag_trends_python["tags2"] == t]
+    # rolling
+    #     temp = list(tag_trends_python[tag_trends_python["tags2"] == t]['questions'].rolling(window=5).mean())
+    # ewm
+        temp = list(tag_trends_python[tag_trends_python["tags2"] == t]['questions'].ewm(span=4).mean())
+        date.extend(list(tt["creation_quarter"]))
+        length = len(temp)
+        if len(temp) >= 4:
+            temp[0] = temp[4]/6
+            temp[1] = temp[4]/4
+            temp[2] = temp[4]/3
+            temp[2] = temp[4]/2
+        tagsTwo.extend(length*[t])
+        questions.extend(temp)
+    new_tag_trends_python = pd.DataFrame()
+    new_tag_trends_python["creation_quarter"] = date
+    new_tag_trends_python["tags2"] = tagsTwo
+    new_tag_trends_python["questions"] = questions
+
+    palette = sns.color_palette("colorblind")
+
+    plt.figure(figsize=(12, 8))
+    ax = sns.lineplot(x=new_tag_trends_python['creation_quarter'].astype(str), y='questions', hue='tags2', data=new_tag_trends_python,palette = palette,lw=3)
+    plt.xlabel('Quarter')
+    plt.ylabel('Questions')
+    plt.title('ML Ops')
+    ax.tick_params(axis='x', labelrotation=90)
---
+++ src/plot_smooth/Quarterly_Question Views.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings("ignore")
+
+def process(tag_trends_python):
+    tagsTwo = []
+    questions = []
+    date = []
+    for t in ['views']:
+        tt = tag_trends_python[t]
+    # rolling
+    #     temp = list(tag_trends_python[tag_trends_python["tags2"] == t]['questions'].rolling(window=5).mean())
+    # ewm
+        temp = list(tt.ewm(span=4).mean())
+        date.extend(list(tag_trends_python["creation_quarter"]))
+        length = len(temp)
+        if len(temp) >= 4:
+            temp[0] = temp[4]/6
+            temp[1] = temp[4]/4
+            temp[2] = temp[4]/3
+            temp[2] = temp[4]/2
+        tagsTwo.extend(length*[t])
+        questions.extend(temp)
+    new_tag_trends_python = pd.DataFrame()
+    new_tag_trends_python["creation_quarter"] = date
+    new_tag_trends_python["category"] = tagsTwo
+    new_tag_trends_python["questions"] = questions
+    new_tag_trends_python
+    # print(new_tag_trends_python)
+    plt.figure(figsize=(25, 8))
+    palette = sns.color_palette("dark")
+    # sns.lineplot(x="time", y="count",hue="action",err_style="bars", ci=68, data=data, palette=palette)
+    ax = sns.lineplot(x=new_tag_trends_python['creation_quarter'].astype(str), y='questions', hue='category', data=new_tag_trends_python, palette=palette, lw=5)
+    plt.xlabel('Quarter')
+    plt.ylabel('Value')
+    plt.title('Question Views')
+    ax.tick_params(axis='x', labelrotation=90)
+    ax.tick_params(axis='x', which='major', labelsize=9)
+    ax.tick_params(axis='y', which='major', labelsize=9)
