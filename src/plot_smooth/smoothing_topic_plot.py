5298c3d36b20690fd84217476311a59dee7070c9
HaoQiuUCSD <h3qiu@ucsd.edu>
2022-11-22 23:59:54 -0800

    topic smoothing plot

--- src/plot_smooth/smoothing_topic_plot.py
+++ src/plot_smooth/smoothing_topic_plot.py
@@ -4,22 +4,28 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
-def process(tag_trends_python):
+def process(tag_trends_python, topic:str, xlabel:str, ylabel:str, span = 5):
+    """_summary_
+
+    Args:
+        tag_trends_python (_type_): _description_
+        title (str): _description_ 
+        xlabel (str): _description_
+        ylabel (str): _description_
+        span (int, optional): _description_. Defaults to 5.
+    """
     a_tags = list(tag_trends_python["tags2"])
-    set(a_tags)
-    
-#     a_tags.remove('pandas')
     tagsTwo = []
     questions = []
     date = []
     for t in a_tags:
-        if t == "pandas":
-            continue
         tt = tag_trends_python[tag_trends_python["tags2"] == t]
+        
     # rolling
     #     temp = list(tag_trends_python[tag_trends_python["tags2"] == t]['questions'].rolling(window=5).mean())
+    
     # ewm
-        temp = list(tag_trends_python[tag_trends_python["tags2"] == t]['questions'].ewm(span=5).mean())
+        temp = list(tag_trends_python[tag_trends_python["tags2"] == t]['questions'].ewm(span = span).mean())
         date.extend(list(tt["creation_quarter"]))
         length = len(temp)
         if len(temp) >= 4:
@@ -33,14 +39,11 @@ def process(tag_trends_python):
     new_tag_trends_python["creation_quarter"] = date
     new_tag_trends_python["tags2"] = tagsTwo
     new_tag_trends_python["questions"] = questions
-    new_tag_trends_python
-    return new_tag_trends_python
 
-new_tag_trends_python = process(tag_trends_python)
-palette = sns.color_palette("colorblind")
-plt.figure(figsize=(12, 8))
-ax = sns.lineplot(x=new_tag_trends_python['creation_quarter'].astype(str), y='questions', hue='tags2', data=new_tag_trends_python,palette = palette,lw=2)
-plt.xlabel('Quarter')
-plt.ylabel('Questions')
-plt.title('Python Packages without Pandas')
-ax.tick_params(axis='x', labelrotation=90)
+    palette = sns.color_palette("colorblind")
+    plt.figure(figsize=(12, 8))
+    ax = sns.lineplot(x=new_tag_trends_python['creation_quarter'].astype(str), y='questions', hue='tags2', data=new_tag_trends_python,palette = palette,lw=2)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(topic)
+    ax.tick_params(axis='x', labelrotation=90)
